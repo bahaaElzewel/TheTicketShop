14c0bd6fb313cee53f5e5fc3bc29358325df4d24
Bahaa <bahaa.zewel@gmail.com>
2025-07-26 14:05:29 +0300

    added the token the gRPC client

--- Services/TicketService.cs
+++ Services/TicketService.cs
@@ -1,4 +1,5 @@
 using EvoTicketingGRPC;
+using Grpc.Core;
 using Grpc.Net.Client;
 
 namespace TheTicketShop.Services;
@@ -6,12 +7,20 @@ namespace TheTicketShop.Services;
 public class TicketService
 {
     private readonly TicketingService.TicketingServiceClient _client;
-    public TicketService(TicketingService.TicketingServiceClient client)
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    public TicketService(TicketingService.TicketingServiceClient client, IHttpContextAccessor httpContextAccessor)
     {
         _client = client;
+        _httpContextAccessor = httpContextAccessor;
+
     }
     public async Task<TicketResponse> GetAllTickets()
     {
+        var jwtToken = _httpContextAccessor.HttpContext?.Request.Headers["Authorization"].ToString().Replace("Bearer ", string.Empty);
+        var headers = new Metadata
+        {
+            { "Authorization", $"Bearer {jwtToken}" }
+        };
         var result = await _client.TicketsAsync(new TicketRequest
         {
             TicketId = 1
