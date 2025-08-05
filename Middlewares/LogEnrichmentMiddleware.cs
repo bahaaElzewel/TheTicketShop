8cbf9e8b99192301ce6983e7de2522a35ed6d54c
Bahaa <bahaa.zewel@gmail.com>
2025-08-05 17:13:46 +0300

    Stop tracking .vs folder and added the OpenTelemetry Implementation and the SeriLog and its Enrichments

---
+++ Middlewares/LogEnrichmentMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Serilog.Context;
+using System.Diagnostics;
+
+namespace TheTicketShop.Middlewares;
+public class LogEnrichmentMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public LogEnrichmentMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        Activity? activity = Activity.Current ?? new Activity("IncomingRequest").Start();
+
+        // RequestId: either extracted from gateway or generated
+        string requestId = context.Request.Headers["X-Request-ID"].FirstOrDefault() ?? Guid.NewGuid().ToString();
+
+        // Push all logging properties
+        using (LogContext.PushProperty("RequestId", requestId))
+        using (LogContext.PushProperty("TraceId", activity?.TraceId.ToString()))
+        using (LogContext.PushProperty("SpanId", activity?.SpanId.ToString()))
+        using (LogContext.PushProperty("ParentSpanId", activity?.ParentSpanId.ToString()))
+        using (LogContext.PushProperty("ServiceName", "TheTicketsShop"))
+        {
+            await _next(context);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,46 @@
 using EvoTicketingGRPC;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Serilog;
 using TheTicketShop.IService;
+using TheTicketShop.Middlewares;
 using TheTicketShop.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Host.UseSerilog();
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+builder.Services.AddOpenTelemetry()
+.ConfigureResource(resource => resource.AddService("TheTicketShop"))
+.WithMetrics(metrics =>
+{
+    metrics
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation();
+    metrics.AddOtlpExporter(options =>
+    {
+        options.Endpoint = new Uri("http://localhost:18890");
+    });
+})
+.WithTracing(tracing =>
+{
+    tracing
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddEntityFrameworkCoreInstrumentation();
+    tracing.AddOtlpExporter(options =>
+    {
+        options.Endpoint = new Uri("http://localhost:18889");
+    });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -16,16 +51,14 @@ builder.Services.AddHttpContextAccessor();
 builder.Services.AddHttpClient();
 builder.Services.AddScoped<IBaseService, BaseService>();
 
-builder.Host.UseSerilog();
-Log.Logger = new LoggerConfiguration()
-    .ReadFrom.Configuration(builder.Configuration).CreateLogger();
-
 builder.Services.AddScoped<TicketService>();
 
 builder.Services.AddGrpcClient<TicketingService.TicketingServiceClient>(o =>
 {
     o.Address = new Uri("https://localhost:7206");
 });
+
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
@@ -36,6 +69,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<LogEnrichmentMiddleware>();
+
 app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
@@ -43,5 +78,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHealthChecks("/health");
 
 app.Run();
